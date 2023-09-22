946835e33ef69bc22c012b8ed1530dcef2e34ba5
Vinícius Chaves <126925449+ChavesVini@users.noreply.github.com>
2023-09-22 20:23:24 -0300

    Add files via upload

--- Engenharia SW/Projetos-main/Projetos-main/matheushedgehog/javaw/src/Bertoti/Sabor.java
+++ Engenharia SW/Projetos-main/Projetos-main/matheushedgehog/javaw/src/Bertoti/Sabor.java
@@ -1,13 +1,36 @@
-import java.util.Arrays;
+package Bertoti;
+
+import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.org.apache.xerces.internal.jaxp.validation.*;  ;
-
-public class ForQueryList {
+public class Sabor {
     public static void main(String[] args) {
-        List<String> QueryList = Arrays.asList("Chocolate", "Morango", "Uva");
-        for (int i = 0; i < QueryList.size(); i++) {
-            System.out.println(QueryList.get(i));
+        List<String> Sabores = new ArrayList<String>();
+        List<String> saborEspecifico = new ArrayList<String>();
+        String saborEscolhido;
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Temos esses sabores: " + "\n");
+
+        Sabores = new ArrayList<String>();
+        Sabores.add("Chocolate");
+        Sabores.add("Uva");
+        Sabores.add("Morango");
+        
+        for (int i = 0; i < Sabores.size(); i++) {
+            System.out.println(Sabores.get(i));
+        }
+
+        System.out.println("\n" + "Qual você escolhe?");
+        saborEscolhido = leitor.nextLine();
+        
+        if (Sabores.contains(saborEscolhido)) {
+            saborEspecifico.add(saborEscolhido);
+            System.out.println(saborEspecifico);
+
+        } else {
+            System.err.println("Sabor errado! Tente de novo!");
         }
     }
 }
